92224127618e4da9c201567080ca5735f09d9b2d
alydinus <alykulovalydin@gmail.com>
2024-09-26 17:53:22 +0600

    monte-carlo method

--- sinx/sinus.f95
+++ sinx/sinus.f95
@@ -1,22 +1,20 @@
 program gptGeneratedSolution
 implicit none
-real(8)::a,b, pie, S1,S2, randomNumber
+real(8)::a,b, pie, S1,S2, randomNumber,S3
 integer::i,M
 i = 1
+S3 = 0.0
 S1 = 0.0
 S2 = 0.0
-M = 100000
+M = 1000
 pie = 4*datan(1.0d0)
 a = pie
 b = 3*pie/2
 do while(i<M)
     randomNumber= (b-a)*rand() + a
-    S1 = sin(randomNumber) + S1
-    S2 = sin(randomNumber)*randomNumber + S2
-    ! print*, S/i
+    S3 = sin(randomNumber) + S3
     i = i + 1
 end do    
-print*,S1/M
-print*,S2/M
+print*,S3/M*(b-a)
 print*,pie
 end program gptGeneratedSolution    
